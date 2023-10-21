9f7b6200e494db388ea6f3a767c98b55ed1fa3bb
LukaL <Lukalursmanashvili398@gmail.com>
2023-10-21 11:08:18 +0400

    Build a navbar

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      x<App />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
